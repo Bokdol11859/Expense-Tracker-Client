fa27d15a3db2eb69bd6517af28e0823038592914
bokdol11859 <2019147551@yonsei.ac.kr>
2023-10-01 21:58:30 +0900

    [Feat] Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { TopNavigationBar } from "@/components/TopNavigationBar.tsx";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
           <div>
             <TopNavigationBar />
             {children}
+            <Analytics />
           </div>
         </div>
       </body>
