5da69fd86dbeb0fa803a31cdcc3414b0c3880085
Bokdol11859 <2019147551@yonsei.ac.kr>
2023-10-22 18:07:44 +0900

    [Feat] Implement useLocalStorage custom hook

---
+++ src/common/hooks/useLocalStorage.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+export function useLocalStorage<T>(
+  key: string,
+  initialValue: T
+): [T, (value: T) => void] {
+  const [storedValue, setStoredValue] = React.useState<T>(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.warn("Error getting local storage key:", key, error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: T) => {
+    try {
+      window.localStorage.setItem(key, JSON.stringify(value));
+      setStoredValue(value);
+    } catch (error) {
+      console.error("Error setting local storage key:", key, error);
+    }
+  };
+
+  React.useEffect(() => {
+    const handleStorageChange = (e: StorageEvent) => {
+      if (e.key === key && e.newValue !== e.oldValue) {
+        setStoredValue(JSON.parse(e.newValue!));
+      }
+    };
+    window.addEventListener("storage", handleStorageChange);
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, [key]);
+
+  return [storedValue, setValue];
+}
